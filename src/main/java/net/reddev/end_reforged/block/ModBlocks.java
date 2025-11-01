59fe1c2f36b7245a0c0a32c7278b6397242f2279
Sir_Reddev <devusserwannes@gmail.com>
2025-11-01 18:36:48 +0100

    ADDED: deep layered end rock layered end rock end moss block deep green end rock green end rock

--- src/main/java/net/reddev/end_reforged/block/ModBlocks.java
+++ src/main/java/net/reddev/end_reforged/block/ModBlocks.java
@@ -22,6 +22,43 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.STONE)));
 
+    public static final Block DEEP_END_SLATE_BLOCK = registerBlock("deep_end_slate_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.STONE)));
+
+    public static final Block GREEN_END_ROCK_BLOCK = registerBlock("green_end_rock_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.STONE)));
+
+    public static final Block DEEP_GREEN_END_ROCK_BLOCK = registerBlock("deep_green_end_rock_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.STONE)));
+
+
+    public static final Block LAYERED_END_ROCK_BLOCK = registerBlock("layered_end_rock_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.STONE)));
+
+    public static final Block DEEP_LAYERED_END_ROCK_BLOCK = registerBlock("deep_layered_end_rock_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.STONE)));
+
+    public static final Block END_MOSS_BLOCK = registerBlock("end_moss_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.MOSS_BLOCK)));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -40,6 +77,13 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.END_SLATE_BLOCK);
             entries.add(ModBlocks.END_NYLIUM_BLOCK);
+            entries.add(ModBlocks.DEEP_END_SLATE_BLOCK);
+            entries.add(ModBlocks.GREEN_END_ROCK_BLOCK);
+            entries.add(ModBlocks.DEEP_GREEN_END_ROCK_BLOCK);
+            entries.add(ModBlocks.END_MOSS_BLOCK);
+            entries.add(ModBlocks.LAYERED_END_ROCK_BLOCK);
+            entries.add(ModBlocks.DEEP_LAYERED_END_ROCK_BLOCK);
+
         });
     }
 }
