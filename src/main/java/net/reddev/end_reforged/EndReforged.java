fca166163e1082d0cf304b8921d550502be00b48
Sir_Reddev <devusserwannes@gmail.com>
2025-11-01 02:57:45 +0100

    Added the first block called "endsoil"

--- src/main/java/net/reddev/end_reforged/EndReforged.java
+++ src/main/java/net/reddev/end_reforged/EndReforged.java
@@ -2,16 +2,17 @@ package net.reddev.end_reforged;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.reddev.end_reforged.block.ModBlocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EndReforged implements ModInitializer {
-    public static final String Mod_ID = "endreforged";
+    public static final String Mod_ID = "end_reforged";
     public static final Logger LOGGER = LoggerFactory.getLogger(Mod_ID);
 
 
 	@Override
 	public void onInitialize() {
-
+        ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/reddev/end_reforged/block/ModBlocks.java
@@ -0,0 +1,39 @@
+package net.reddev.end_reforged.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.reddev.end_reforged.EndReforged;
+
+public class ModBlocks {
+
+    public static final Block ENDSOIL_BLOCK = registerBlock("endsoil_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.MUD)));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(EndReforged.Mod_ID, name), block);
+    }
+
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(EndReforged.Mod_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        EndReforged.LOGGER.info("Registering ModBlocks for " + EndReforged.Mod_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.ENDSOIL_BLOCK);
+        });
+    }
+}
